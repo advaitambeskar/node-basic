89f26823eb3811d22a7a5f8b07940cef1b24189a
advaitambeskar <ambeskaradvait@gmail.com>
2018-05-26 04:33:01 -0400

    Added Sync FileSystem

--- firstIO.js
+++ firstIO.js
@@ -1,1 +1,5 @@
-var fs = require('fs');
+let fs = require('fs');
+let fileContent = fs.readFileSync(process.argv[2]);
+let toString = fileContent.toString();
+let toArray = toString.split('\n');
+console.log(toArray.length-1);
