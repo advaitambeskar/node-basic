394ba9d895ebc90cae4c6b88c91b5813524f5ce3
advaitambeskar <ambeskaradvait@gmail.com>
2018-05-29 05:40:09 -0400

    Added Async Server Request

--- juggle-async.js
+++ juggle-async.js
@@ -1,12 +1,29 @@
 let concat_stream = require('concat-stream');
 let http = require('http');
-let data = undefined;
-for(i = 4; i > 1; i--){
-    url = process.argv[i];
-    http.get(url, function urlRequest(response){
-        response.setEncoding('utf8');
-        response.pipe(concat_stream(function (data){
-            console.log(data);
+let urlList = [];
+let responseList = [];
+let count = 0;
+
+let i = 0;
+for(i = 2; i< process.argv.length; i++){
+    urlList.push(process.argv[i]);
+}
+
+function readResponse(index){
+    http.get(urlList[index], function(response){
+        response.pipe(concat_stream(function(data){
+            responseList[index] = data.toString();
+            count ++;
+
+            if (count == 3){
+                responseList.forEach(element => {
+                    console.log(element);
+                });
+            }
         }));
-    });
+    })
+}
+
+for(i = 0; i< urlList.length; i++){
+    readResponse(i);
 }
